12653f8fd99103de5e58ab924e28f26a2f8d3469
bal7hazar <bal7hazar@proton.me>
2025-01-24 12:12:02 +0100

    🐛 Fix tokens

--- client/src/components/navigation/index.tsx
+++ client/src/components/navigation/index.tsx
@@ -11,9 +11,9 @@ import { Link, useLocation } from "react-router-dom";
 export function Navigation() {
   return (
     <div className="flex overflow-hidden shrink-0 gap-x-4">
-      <Item Icon={CoinsIcon} variant="inventory" title="Inventory" />
+      <Item Icon={CoinsIcon} variant="inventory" title="Assets" />
       <Item Icon={TrophyIcon} variant="achievements" title="Achievements" />
-      <Item Icon={ClockIcon} variant="activity" title="Activity" />
+      <Item Icon={ClockIcon} variant="activity" title="Activity" disabled />
     </div>
   );
 }
@@ -22,10 +22,12 @@ function Item({
   Icon,
   title,
   variant,
+  disabled = false,
 }: {
   Icon: React.ComponentType<StateIconProps>;
   title: string;
   variant: string;
+  disabled?: boolean;
 }) {
   const { pathname } = useLocation();
 
@@ -39,8 +41,9 @@ function Item({
       className={cn(
         "flex gap-2 px-4 py-3 h-11 justify-center items-center cursor-pointer hover:opacity-[0.8] rounded border border-secondary",
         isActive ? "bg-secondary" : "bg-background",
+        disabled && "opacity-50 cursor-not-allowed",
       )}
-      to={`/${variant}`}
+      to={disabled ? "#" : `/${variant}`}
     >
       <Icon size="sm" variant={isActive ? "solid" : "line"} />
       <span>{title}</span>
