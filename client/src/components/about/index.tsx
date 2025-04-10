3281fed61902dc4de19dc6216ae1fec2d9751ff2
bal7hazar <bal7hazar@proton.me>
2025-04-10 16:41:25 +0200

    🐛 Fix game description

--- client/src/components/about/index.tsx
+++ client/src/components/about/index.tsx
@@ -1,12 +1,13 @@
 import Details from "./details";
 import Media from "./media";
 import Metrics from "./metrics";
+import { GameModel } from "@bal7hazar/arcade-sdk";
 
-export function About() {
+export function About({ game }: { game: GameModel }) {
   return (
     <div className="flex flex-col gap-4 py-4">
       <Media />
-      <Details content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." />
+      <Details content={game.metadata.description || ""} />
       <Metrics />
     </div>
   );
--- client/src/components/scenes/about.tsx
+++ client/src/components/scenes/about.tsx
@@ -1,5 +1,21 @@
 import { About } from "@/components/about";
+import { useArcade } from "@/hooks/arcade";
+import { useProject } from "@/hooks/project";
+import { GameModel } from "@bal7hazar/arcade-sdk";
+import { useMemo } from "react";
 
 export const AboutScene = () => {
-  return <About />;
+  const { games } = useArcade();
+
+  const { project, namespace } = useProject();
+
+  const game: GameModel | undefined = useMemo(() => {
+    return Object.values(games).find(
+      (game) => game.namespace === namespace && game.config.project === project,
+    );
+  }, [games, project, namespace]);
+
+  if (!game) return null;
+
+  return <About game={game} />;
 };
