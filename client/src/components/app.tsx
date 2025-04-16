8a20f86398e1dbd732616ae21988ee728cb9b651
bal7hazar <bal7hazar@proton.me>
2025-04-16 18:49:14 +0200

    🐛 Fix top padding

--- client/src/components/app.tsx
+++ client/src/components/app.tsx
@@ -75,7 +75,7 @@ const AppContent = () => {
     >
       <div
         className={cn(
-          "lg:w-[1112px] pt-2 lg:pt-8 pb-6 gap-3 lg:gap-8 flex items-stretch m-auto h-full overflow-clip",
+          "lg:w-[1112px] lg:pt-8 pb-6 gap-3 lg:gap-8 flex items-stretch m-auto h-full overflow-clip",
           "transition-all duration-300 ease-in-out",
         )}
       >
