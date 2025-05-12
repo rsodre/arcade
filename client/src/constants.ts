7cb0893abfc8cb8e027f45cd1b7ffde65dfe920d
bal7hazar <bal7hazar@proton.me>
2025-05-12 21:16:14 +0200

    🐛 Update preset

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,7 +1,7 @@
 // Constants for the project
 export const DEFAULT_PROJECT: string = "arcadev2";
 export const DEFAULT_NAMESPACE: string = "ARCADE";
-export const DEFAULT_PRESET: string = "cartridge";
+export const DEFAULT_PRESET: string = "arcade";
 // Constants required to query the achievement events
 export const TROPHY: string = "TrophyCreation";
 export const PROGRESS: string = "TrophyProgression";
