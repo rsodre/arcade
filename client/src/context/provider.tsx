70d4f8c19fc61fa064dcec2b3eb1a0aa44cff2bd
Nuriman Quddus <65181897+nrmnqdds@users.noreply.github.com>
2025-05-12 22:07:24 +0800

    Add posthog provider (#46)

--- client/src/context/provider.tsx
+++ client/src/context/provider.tsx
@@ -11,38 +11,41 @@ import { ActivitiesProvider } from "./activities";
 import { DiscoversProvider } from "./discovers";
 import { MetricsProvider } from "./metrics";
 import { OwnershipsProvider } from "./ownerships";
+import { PostHogProvider } from "./posthog";
 import { SidebarProvider } from "./sidebar";
 
 export function Provider({ children }: PropsWithChildren) {
   const queryClient = new QueryClient();
 
   return (
-    <CartridgeAPIProvider
-      url={`${import.meta.env.VITE_CARTRIDGE_API_URL}/query`}
-    >
-      <IndexerAPIProvider credentials="omit">
-        <QueryClientProvider client={queryClient}>
-          <ArcadeProvider>
-            <StarknetProvider>
-              <OwnershipsProvider>
-                <CollectionProvider>
-                  <TokenProvider>
-                    <AchievementProvider>
-                      <DiscoversProvider>
-                        <ActivitiesProvider>
-                          <MetricsProvider>
-                            <SidebarProvider>{children}</SidebarProvider>
-                          </MetricsProvider>
-                        </ActivitiesProvider>
-                      </DiscoversProvider>
-                    </AchievementProvider>
-                  </TokenProvider>
-                </CollectionProvider>
-              </OwnershipsProvider>
-            </StarknetProvider>
-          </ArcadeProvider>
-        </QueryClientProvider>
-      </IndexerAPIProvider>
-    </CartridgeAPIProvider>
+    <PostHogProvider>
+      <CartridgeAPIProvider
+        url={`${import.meta.env.VITE_CARTRIDGE_API_URL}/query`}
+      >
+        <IndexerAPIProvider credentials="omit">
+          <QueryClientProvider client={queryClient}>
+            <ArcadeProvider>
+              <StarknetProvider>
+                <OwnershipsProvider>
+                  <CollectionProvider>
+                    <TokenProvider>
+                      <AchievementProvider>
+                        <DiscoversProvider>
+                          <ActivitiesProvider>
+                            <MetricsProvider>
+                              <SidebarProvider>{children}</SidebarProvider>
+                            </MetricsProvider>
+                          </ActivitiesProvider>
+                        </DiscoversProvider>
+                      </AchievementProvider>
+                    </TokenProvider>
+                  </CollectionProvider>
+                </OwnershipsProvider>
+              </StarknetProvider>
+            </ArcadeProvider>
+          </QueryClientProvider>
+        </IndexerAPIProvider>
+      </CartridgeAPIProvider>
+    </PostHogProvider>
   );
 }
