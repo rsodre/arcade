099a16cd1d93545b6f160a2a063f9673d7a1ada1
Brother MartianGreed <valentin@pupucecorp.com>
2025-09-26 09:46:17 +0200

    chore: remove unused code (#174)

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,8 +1,6 @@
 // Constants for the project
 export const DEFAULT_PROJECT: string = "arcade-main";
 export const DEFAULT_TOKENS_PROJECT: string = "c7e-arcade-tokens";
-export const DEFAULT_NAMESPACE: string = "ARCADE";
-export const DEFAULT_PRESET: string = "arcade";
 // Constants required to query the achievement events
 export const TROPHY: string = "TrophyCreation";
 export const PROGRESS: string = "TrophyProgression";
--- client/src/store.ts
+++ client/src/store.ts
@@ -1,42 +1,5 @@
 import { create } from "zustand";
-import { Discover } from "./hooks/discovers-fetcher";
 import { Token } from "@dojoengine/torii-wasm";
-
-type State = {
-  events: { [project: string]: Discover[] };
-};
-type Actions = {
-  addEvents: (events: { [project: string]: Discover[] }) => void;
-  getAllEvents: (editions: string[]) => Discover[];
-  getFollowingEvents: (editions: string[], addr: string[]) => Discover[];
-};
-
-export const useEventStore = create<State & Actions>((set, get) => ({
-  events: {},
-  addEvents: (evts) =>
-    set(() => {
-      const events = get().events;
-      for (const [project, discovers] of Object.entries(evts)) {
-        events[project] = discovers;
-      }
-      return {
-        events,
-      };
-    }),
-  getAllEvents: (editions: string[]) => {
-    const events = get().events;
-    return Object.values(events)
-      .flatMap((e) => e)
-      .filter((e) => e.name !== undefined && editions.includes(e.project))
-      .sort((a, b) => b.timestamp - a.timestamp);
-  },
-  getFollowingEvents: (editions: string[], addr: string[]) => {
-    const events = get().getAllEvents(editions);
-    return events.filter((e) => addr.includes(e.address));
-  },
-}));
-
-export type MarketplaceCollection = Token & { count: number; project: string };
 
 export type Contract = {
   project: string;
