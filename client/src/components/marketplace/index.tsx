d827ff887dc364cc0b8db9499d93ae21c795799f
ayushtom <tomar2585@gmail.com>
2025-09-30 22:35:25 +0530

    Revert "Fix BigInt conversion error in CollectibleCard"
    This reverts commit eb2d74d280b1c251f20b182b20e4dabf2009821f.

--- client/src/components/marketplace/index.tsx
+++ client/src/components/marketplace/index.tsx
@@ -205,7 +205,7 @@ function Item({
       <CollectibleCard
         title={collection.name}
         image={collection.image}
-        totalCount={Number(collection.totalSupply)}
+        totalCount={collection.totalSupply as unknown as number}
         selectable={false}
         listingCount={listingCount}
         onClick={handleClick}
