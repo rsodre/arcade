bb3d13cbe484d9e10ad1b1f983d7e29853a5d69d
Bal7hazar <bal7hazar@proton.me>
2025-11-10 11:06:02 +0100

    fix: udpate torii tokens (#219)

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -3,7 +3,7 @@ import { getChecksumAddress } from "starknet";
 // Constants for the project
 export const DEFAULT_PROJECT: string = "arcade-main";
 export const DEFAULT_PRESET: string = "arcade";
-export const DEFAULT_TOKENS_PROJECT: string = "c7e-arcade-tokens";
+export const DEFAULT_TOKENS_PROJECT: string = "c7e-tokens-starknet";
 export const BLACKLISTS: string[] = [
   getChecksumAddress(
     "0x07fc27ad206a82c097260f92d822de9bf205095205747fadef9c596714491437",
